e185c0490d36c689cb70ff4c19f25ea38855f1f4
毛毛 <20774746@qq.com>
2017-11-01 17:05:44 +0800

    顶顶顶
    顶顶顶

--- prj/android/TntPlayer/app/src/main/java/com/gwgz/tntplayer/MainActy.java
+++ prj/android/TntPlayer/app/src/main/java/com/gwgz/tntplayer/MainActy.java
@@ -1,14 +1,17 @@
 package com.gwgz.tntplayer;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.webkit.JsPromptResult;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -37,59 +40,65 @@ public class MainActy extends AppCompatActivity {
         wst.setAllowFileAccessFromFileURLs(true);
         wst.setJavaScriptCanOpenWindowsAutomatically(true);
 
-        wv.setWebChromeClient(new WebChromeClient() {
-                                        // 拦截输入框(原理同方式2)
-                                        // 参数message:代表promt（）的内容（不是url）
-                                        // 参数result:代表输入框的返回值
-                                        @Override
-                                        public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
-                                            // 根据协议的参数，判断是否是所需要的url(原理同方式2)
-                                            // 一般根据scheme（协议格式） & authority（协议名）判断（前两个参数）
-                                            //假定传入进来的 url = "js://webview?arg1=111&arg2=222"（同时也是约定好的需要拦截的）
+        wv.setWebChromeClient(
+                new WebChromeClient(){
+                    @Override
+                    public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+                        Uri uri = Uri.parse(message);
+                        if ( uri.getScheme().equals("js")) {
+                            if (uri.getAuthority().equals("cpp")) {
+                                System.out.println("js调用了Android的方法");
+                                HashMap<String, String> params = new HashMap<>();
+                                Set<String> collection = uri.getQueryParameterNames();
+                                result.confirm("js调用了Android的方法成功啦");
+                            }
+                            return true;
+                        }
+                        return super.onJsPrompt(view, url, message, defaultValue, result);
+                    }
 
-                                            Uri uri = Uri.parse(message);
-                                            // 如果url的协议 = 预先约定的 js 协议
-                                            // 就解析往下解析参数
-                                            if ( uri.getScheme().equals("js")) {
+                    @Override
+                    public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                        return super.onJsAlert(view, url, message, result);
+                    }
 
-                                                // 如果 authority  = 预先约定协议里的 webview，即代表都符合约定的协议
-                                                // 所以拦截url,下面JS开始调用Android需要的方法
-                                                if (uri.getAuthority().equals("cpp")) {
-
-                                                    //
-                                                    // 执行JS所需要调用的逻辑
-                                                    System.out.println("js调用了Android的方法");
-                                                    // 可以在协议上带有参数并传递到Android上
-                                                    HashMap<String, String> params = new HashMap<>();
-                                                    Set<String> collection = uri.getQueryParameterNames();
-
-                                                    //参数result:代表消息框的返回值(输入值)
-                                                    result.confirm("js调用了Android的方法成功啦");
-                                                }
-                                                return true;
-                                            }
-                                            return super.onJsPrompt(view, url, message, defaultValue, result);
-                                        }
-
-// 通过alert()和confirm()拦截的原理相同，此处不作过多讲述
-
-                                        // 拦截JS的警告框
-                                        @Override
-                                        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-                                            return super.onJsAlert(view, url, message, result);
-                                        }
-
-                                        // 拦截JS的确认框
-                                        @Override
-                                        public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
-                                            return super.onJsConfirm(view, url, message, result);
-                                        }
-                                    }
+                    @Override
+                    public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+                        return super.onJsConfirm(view, url, message, result);
+                    }
+                }
         );
 
+        wv.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.i("webview", "url = "+url);
+                view.loadUrl(url);
+                return true;
+            }
+            @Override
+            public void onReceivedError(WebView view, int errorCode,
+                                        String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                //这里进行无网络或错误处理，具体可以根据errorCode的值进行判断，做跟详细的处理。
+                view.loadUrl("file:///android_asset/www/error.html");
+            }
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                Log.i("onPageStarted", url);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.i("onPageFinished", url);
+                super.onPageFinished(view, url);
+            }
+        });
 
 
-        wv.loadUrl("file:///android_asset/www/index.html");
+
+        wv.loadUrl("file:///android_asset/www/index1.html");
 
 
     }
