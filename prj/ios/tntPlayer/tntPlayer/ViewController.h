5250d670429fc8e52de1f07fc87ae7d3efa700d5
xiny120 <20774746@qq.com>
2017-12-22 03:30:41 -0800

    ios
    ios new project

---
+++ prj/ios/tntPlayer/tntPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tntPlayer
+//
+//  Created by zcl on 2017/12/22.
+//  Copyright © 2017年 zcl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
